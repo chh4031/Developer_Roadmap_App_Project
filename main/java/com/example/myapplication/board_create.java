03ffe3899af46dbe564755d30d32a13f6e40dad2
chh4031 <chh4031@gmail.com>
2023-05-16 19:16:10 +0900

    자바_게시글작성

---
+++ main/java/com/example/myapplication/board_create.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class board_create extends AppCompatActivity{
+
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.board_create);
+    }
+
+}
