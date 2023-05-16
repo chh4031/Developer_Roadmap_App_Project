e53bc7096a378724ff86c66e89952976f5c65b85
chh4031 <chh4031@gmail.com>
2023-05-16 19:15:46 +0900

    기타 XML들

--- main/java/com/example/myapplication/MainActivity.java
+++ main/java/com/example/myapplication/MainActivity.java
@@ -9,19 +9,49 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btn1;
+    Button main_btn1;
+    Button main_btn2;
+    Button main_btn3;
+    Button main_btn4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn1 = (Button) findViewById(R.id.btn1);
+        main_btn1 = (Button) findViewById(R.id.main_btn1);
+        main_btn2 = (Button) findViewById(R.id.main_btn2);
+        main_btn3 = (Button) findViewById(R.id.main_btn3);
+        main_btn4 = (Button) findViewById(R.id.main_btn4);
 
-        btn1.setOnClickListener(new View.OnClickListener() {
+        main_btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), MainActivity2.class);
+                Intent intent = new Intent(getApplicationContext(), community.class);
+                startActivity(intent);
+            }
+        });
+
+        main_btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), roadmap.class);
+                startActivity(intent);
+            }
+        });
+
+        main_btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), login.class);
+                startActivity(intent);
+            }
+        });
+
+        main_btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), singup.class);
                 startActivity(intent);
             }
         });
