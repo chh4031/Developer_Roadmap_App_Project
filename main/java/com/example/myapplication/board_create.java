cb3c8b2c0dd269b76ba2f61cd03d8b13d71edf6c
chh4031 <chh4031@gmail.com>
2023-05-26 22:58:59 +0900

    글쓰기 수정

--- main/java/com/example/myapplication/board_create.java
+++ main/java/com/example/myapplication/board_create.java
@@ -2,13 +2,25 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class board_create extends AppCompatActivity{
+
+    TextView this_userB;
 
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.board_create);
+
+        this_userB = (TextView) findViewById(R.id.this_userB);
+
+        Intent putdata = getIntent();
+        String thisId = putdata.getStringExtra("thisId");
+        this_userB.setText(thisId);
     }
 
 }
